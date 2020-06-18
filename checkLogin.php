093b7ca5e27d3be05283aade63091951dace4af0
dominhngoc123 <61538590+dominhngoc123@users.noreply.github.com>
2020-06-18 16:23:45 +0700

    Update checkLogin.php

--- checkLogin.php
+++ checkLogin.php
@@ -13,7 +13,7 @@
 
 		$connection = pg_connect("host=ec2-52-20-248-222.compute-1.amazonaws.com dbname=de5ugtq8r5s3s4 user=rzbltrsfgxfmpi password=f1609ea5b6f86c01655cd98bd6dad7df491506a6aa7f5351a15fa6f1045c504b");
 		$username = $_POST["txtUsername"];
-		$password = $_POST["txtPassword"]);
+		$password = $_POST["txtPassword"];
 		$sql = "SELECT * FROM tblAdmin WHERE _user = '".$username."' AND _password = '".$password."'";
 		$result = pg_query($connection, $sql);
 		$row = pg_num_rows($result);
