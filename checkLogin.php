a5d197a6d18e9c18e1196521a269eed89b80d173
dominhngoc123 <61538590+dominhngoc123@users.noreply.github.com>
2020-06-18 16:16:11 +0700

    Update checkLogin.php

--- checkLogin.php
+++ checkLogin.php
@@ -11,14 +11,12 @@
 	else
 	{
 
-		$connection = mysqli_connect("localhost","root","","trainingfptcompany");
-		$username = stripslashes($_POST["txtUsername"]);
-		$username = mysqli_real_escape_string($connection, $username);
-		$password = stripslashes($_POST["txtPassword"]);
-		$password = mysqli_real_escape_string($connection, $password);
+		$connection = pg_connect("host=ec2-52-20-248-222.compute-1.amazonaws.com dbname=de5ugtq8r5s3s4 user=rzbltrsfgxfmpi password=f1609ea5b6f86c01655cd98bd6dad7df491506a6aa7f5351a15fa6f1045c504b");
+		$username = $_POST["txtUsername"];
+		$password = $_POST["txtPassword"]);
 		$sql = "SELECT * FROM tblAdmin WHERE _user = '".$username."' AND _password = '".$password."'";
-		$result = mysqli_query($connection, $sql) or die(mysqli_errno($connection));
-		$row = mysqli_num_rows($result);
+		$result = pg_query($connection, $sql);
+		$row = pg_num_rows($result);
 		if ($row == 1)
 		{
 			?>
